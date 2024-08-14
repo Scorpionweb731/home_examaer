7d8234a19bbf338495cd8ee4f8b1291b145d10e4
Mayur Patel <patelweb6022@gmail.com>
2024-08-15 04:15:31 +0530

    Update career_form_submission.js 2

--- assets/js/career_form_submission.js
+++ assets/js/career_form_submission.js
@@ -1,7 +1,6 @@
 
 function validateform(){
     let hidden_file = document.querySelector('#hidden_file');
-    console.log(hidden_file)
     if (hidden_file.files.length === 0) {
       alert('Please upload a resume.');
       return false; // Prevent form submission
