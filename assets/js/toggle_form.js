ae574a80d16e0627f9a1eafc331344eae2638833
Mayur Patel <patelweb6022@gmail.com>
2024-09-26 11:35:56 +0530

    Update toggle_form.js  toggle btn bg update js file

--- assets/js/toggle_form.js
+++ assets/js/toggle_form.js
@@ -5,10 +5,10 @@ let contactbutton = document.querySelectorAll('.showContactButton')
 // JavaScript to toggle visibility of career and contact forms with smooth animation
 carrerbutton.forEach((btn)=>{
   btn.addEventListener('click', function() {
-    carrerbutton[1].style.backgroundColor = 'white';
-    carrerbutton[1].style.color = '#7a6ad8';
-    contactbutton[1].style.backgroundColor = '#7a6ad8';
-    contactbutton[1].style.color = 'white';
+    carrerbutton[1].style.backgroundColor = '#7a6ad8';
+    carrerbutton[1].style.color = 'white';
+    contactbutton[1].style.backgroundColor = 'white';
+    contactbutton[1].style.color = '#7a6ad8';
     document.getElementById('career').classList.add('show');
     document.getElementById('contact').classList.remove('show');
     contactLink.setAttribute('href', '#career');
@@ -18,10 +18,10 @@ carrerbutton.forEach((btn)=>{
 
 contactbutton.forEach((btn)=>{
   btn.addEventListener('click', function() {
-    contactbutton[1].style.backgroundColor = 'white';
-    contactbutton[1].style.color = '#7a6ad8';
-    carrerbutton[1].style.backgroundColor = '#7a6ad8';
-    carrerbutton[1].style.color = 'white';
+    contactbutton[1].style.backgroundColor = '#7a6ad8';
+    contactbutton[1].style.color = 'white';
+    carrerbutton[1].style.backgroundColor = 'white';
+    carrerbutton[1].style.color = '#7a6ad8';
     document.getElementById('contact').classList.add('show');
     document.getElementById('career').classList.remove('show');
     careerLink.setAttribute('href', '#contact');
